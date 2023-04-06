d78e15d8ce1dce03e40f041a9255ab35576417f7
nikita <nikita.zavgorodnii+gh@shelf.io>
2023-04-06 22:26:55 +0100

    zsh: aliases

---
+++ aliases.zsh
@@ -0,0 +1,16 @@
+
+alias ea="source venv/bin/activate"
+alias vea="source ~/.virtualenvs/$(basename $PWD)/bin/activate"
+alias ed="deactivate"
+
+# serverless
+alias sls=serverless
+
+vij() {
+	vim -c ':%!jq' -c 'set filetype=json' $1
+}
+
+
+alias yaml2js="python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)'"
+alias js2yaml="python -c 'import sys, yaml, json; yaml.dump(json.load(sys.stdin), sys.stdout, indent=4)'"
+
