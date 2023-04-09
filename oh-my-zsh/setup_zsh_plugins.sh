d53995ce17a33de5e5925d1abf7e23cf17b96d0f
nikitajz <nikitajz@users.noreply.github.com>
2023-04-09 14:22:55 +0100

    refactor setup plugins

--- oh-my-zsh/setup_zsh_plugins.sh
+++ oh-my-zsh/setup_zsh_plugins.sh
@@ -3,44 +3,26 @@
 # https://github.com/romkatv/zsh-bench
 
 OMZ_PLUGINS=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
-if [[ ! -e $OMZ_PLUGINS/zsh-autosuggestions ]]; then
-  echo "Cloning repositories for zsh plugins..."
-  echo "oh-my-zsh custom path: ${OMZ_PLUGINS}"
-fi
 
-function zcompile-many() {
-  local f
-  for f; do zcompile -R -- "$f".zwc "$f"; done
+# List of plugin name and repository tuples (github is assumed by default)
+plugins=(
+  ("jq" "reegnz/jq-zsh-plugin")
+  ("powerlevel10k", "romkatv/powerlevel10k")
+)
+
+function clone_plugin_repo() {
+  for plugin in "$@:"; do
+    plugin_name="${plugin[0]}"
+    repo_url="${plugin[1]}"
+   
+    if [[ "$repo_url" != http* ]]; then
+      repo_url="https://github.com/$repo_url.git"
+    fi
+    
+    if [[ ! -e "$OMZ_PLUGINS/$plugin_name" ]]; then
+      echo "Cloning the repo for plugin ${plugin_name}"
+      git clone --depth=1 "$repo_url" "$OMZ_PLUGIN/$plugin_name"
+    fi
+  done
 }
-
-# omz plugins
-# Clone and compile to wordcode missing plugins.
-if [[ ! -e $OMZ_PLUGINS/zsh-autosuggestions ]]; then
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $OMZ_PLUGINS/zsh-autosuggestions
-#  zcompile-many $OMZ_PLUGINS/zsh-autosuggestions/{zsh-autosuggestions.zsh,src/**/*.zsh}
-fi
-
-# zsh-autosuggestion should go before zsh-syntax-highlighting
-if [[ ! -e $OMZ_PLUGINS/zsh-syntax-highlighting ]]; then
-  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ_PLUGINS/zsh-syntax-highlighting
-#  zcompile-many $OMZ_PLUGINS/zsh-syntax-highlighting/{zsh-syntax-highlighting.zsh,highlighters/*/*.zsh}
-fi
-
-if [[ ! -e $OMZ_PLUGINS/zsh-completions ]]; then
-  git clone --depth=1 https://github.com/zsh-users/zsh-completions.git $OMZ_PLUGINS/zsh-completions
-#  zcompile-many $OMZ_PLUGINS/zsh-completions/zsh-completions.plugins.zsh
-fi
-
-if [[ ! -e $OMZ_PLUGINS/powerlevel10k ]]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $OMZ_PLUGINS/powerlevel10k
-#  make -C $OMZ_PLUGINS/powerlevel10k pkg
-fi
-
-if [[ ! -e $OMZ_PLUGINS/jq ]]; then
-  git clone --depth=1 https://github.com/reegnz/jq-zsh-plugin.git $OMZ_PLUGINS/jq
-#  zcompile-many $OMZ_PLUGINS/jq/**/*.zsh
-fi
-
-[[ ~/.zcompdump.zwc -nt ~/.zcompdump ]] || zcompile-many ~/.zcompdump
-unfunction zcompile-many
 
