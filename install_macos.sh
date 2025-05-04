568382203f5809d0c02c46b734fe4092f632f0a1
Nikita <12535180+nikitajz@users.noreply.github.com>
2025-05-04 01:35:35 +0100

    homebrew: update apps; remove pyenv

--- install_macos.sh
+++ install_macos.sh
@@ -114,26 +114,7 @@ install_brew_dependencies() {
   print_step "Installing brew dependencies from Brewfile"
 
   brew update
-  brew tap homebrew/bundle
   brew bundle --file $DOTFILES/Brewfile
-}
-
-# Use pyenv to manage python versions
-install_pyenv() {
-  if ! command -v pyenv >/dev/null; then
-    print_step "Installing Python using pyenv"
-    # verify zlib and sqlite3 are installed (required for pyenv)
-    echo '[[ ! -f ~/.profile ]] || source ~/.profile' >>~/.bash_profile
-    pyenv install 3.10
-    pyenv global 3.10
-  else
-    print_warning "Skipping pyenv, already installed"
-  fi
-}
-
-install_iterm2_integration() {
-  print_step "Installing iTerm2 shell integration"
-  curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
 }
 
 install_fzf() {
@@ -163,8 +144,6 @@ main() {
   config_dotfiles
   install_homebrew
   install_brew_dependencies
-  install_pyenv
-  install_iterm2_integration
   install_fzf
   install_oh_my_zsh
 }
